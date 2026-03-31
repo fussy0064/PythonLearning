afc91e376f10332bfdd3e23662a53aa542eaf013
fussy0064 <fussy0064@github.com>
2026-03-31 22:36:39 +0300

    Added exercise files and reference material

---
+++ Variable.py
@@ -0,0 +1,24 @@
+# A variable Stoer Data. 
+# Yoy don`t Need to Decleare Variable in Python Because It is Automatic
+
+name = "Fussy"
+Age = 20
+height = 5.14
+is_student = True
+
+#Printing OutPut 
+print (name) #Fussy
+print (type(Age)) # <class 'int'>
+print (Age) #20
+
+
+#Data Type in Python Are;-
+
+#int = Only Whole Numbers e.g 20
+#str = Text e.g "Fussy"
+#Bool = Only True and False
+#list = Ordered Collection e.g [1, 2, 3]
+# tuple = imutable list e.g (10, 20) 
+#dict = Key value Pair e.g {"a":1}
+#set = Unique items only {1,2,3}
+
---
+++ new.py
@@ -0,0 +1,7 @@
+# The First Programe in Learning 
+print ("Hello World")
+print ("My name is Python") 
+
+# OutPut 
+# Hello World
+# My name is Python
